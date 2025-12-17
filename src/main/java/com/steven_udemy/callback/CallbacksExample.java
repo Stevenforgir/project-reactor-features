6d6252d17227831693319758e09ec54f3c76fa54
StevenUninorte <stevenforgir@gmail.com>
2025-12-17 12:08:20 -0500

    adding the most used callbacks

---
+++ src/main/java/com/steven_udemy/callback/CallbacksExample.java
@@ -0,0 +1,27 @@
+package com.steven_udemy.callback;
+
+import com.steven_udemy.database.Database;
+import com.steven_udemy.models.Videogame;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
+@Slf4j
+public class CallbacksExample {
+
+    /**
+     *
+     * Todos los callbacks comienzan con do...
+     * @return
+     */
+    public static Flux<Videogame> callbacks(){
+        return Database.getDataAsFlux()
+                .doOnSubscribe(subs -> log.info("[doonSubscribe]: {}", subs))
+                .doOnRequest(n -> log.info("[doOnRequest]: {}", n))
+                .doOnNext(v -> log.info("[doOnNext]: {}", v.getName()))
+                .doOnCancel(() -> log.warn("[doOnCancel]"))
+                .doOnError(error -> log.error("[doOnError]: {}", error.getMessage()))
+                .doOnComplete(() -> log.info("[doOnComplete]: Completed successfully"))
+                .doOnTerminate(() -> log.info("[doOnTerminate]: Terminated"))
+                .doFinally(signalType -> log.warn("[doFinally]: {}", signalType));
+    }
+}
